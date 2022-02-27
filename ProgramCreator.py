2ff9e4a4dc51a46f1d4c8657cf453aef8cb8b030
MoeedCh <moeedchaudhry15@gmail.com>
2022-02-26 20:27:05 -0500

    hi

--- ProgramCreator.py
+++ ProgramCreator.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 #from sqlalchemy import desc
 
 questionnaire()
+############################## CONNECT TO DATABASE #####################################333
 
 app = Flask(__name__)
 
@@ -21,7 +22,11 @@ class Accessories(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     muscle = db.Column(db.String)
     name = db.Column(db.String)
-    
+
+############################ END DATABASE STUFF ###############################################
+
+chestExcercise = Accessories.query.filter_by(muscle = 'chest').order_by(Accessories.name).all()
+
 
 #takes one arguement which is the file name
 workbook = xlsxwriter.Workbook('moeed.xlsx')
@@ -29,41 +34,44 @@ workbook = xlsxwriter.Workbook('moeed.xlsx')
 #adds new worksheet
 worksheet = workbook.add_worksheet()
 
-# Use the worksheet object to write
-# data via the write() method.
-#worksheet.write('A1', 'Hello..')
-#worksheet.write('B1', 'Geeks')
-#worksheet.write('C1', 'For')
-#worksheet.write('D1', 'Geeks')
-
-# Set up some formats to use.
+######################################## FORMATS FOR WORKBOOK #################################################
 red = workbook.add_format({'color': 'red'})
 blue = workbook.add_format({'color': 'blue'})
 cell_format = workbook.add_format({'align': 'center',
                                    'valign': 'vcenter',
                                    'border': 2, 'bold': True})
-weekCellFormat = workbook.add_format({'align': 'center',
-                                   'valign': 'vcenter','bold': True, })
-weekCellFormat.set_font_size(10)
 
-# We can only write simple types to merged ranges so we write a blank string.
+weekCellFormat = workbook.add_format({'align': 'center',
+                                   'valign': 'vcenter','bold': True, })#Week cell formatting
+weekCellFormat.set_font_size(20)#increases size of text in week cell
+
+dayCellFormat = workbook.add_format({'align': 'center',
+                                   'valign': 'vcenter','bold': True, 'border':2 })# day cell formatting
+
+movementFormat = workbook.add_format({'align': 'center',
+                                   'valign': 'vcenter','bold': True})
+
+######################################### END FORMATS FOR WORKBOOK #############################################
+
+######################################### Writing to Cells #####################################################
 worksheet.merge_range('A1:K3', "", cell_format)#-----WEEK X
-worksheet.write('A1', u'DAY 1',cell_format)
+worksheet.write('A1', u'WEEK 1',weekCellFormat)
 
 
 worksheet.merge_range('A5:B5', "", cell_format)#-----DAY X
-worksheet.merge_range('D5:E5', "", cell_format)#-----DAY X
-worksheet.merge_range('G5:H5', "", cell_format)#-----DAY X
-worksheet.merge_range('J5:K5', "", cell_format)#-----DAY X
+worksheet.write('A5', u'DAY 1',dayCellFormat)
+worksheet.write(chestExcercise, movementFormat)
 
-# We then overwrite the first merged cell with a rich string. Note that we
-# must also pass the cell format used in the merged cells format at the end.
-worksheet.write_rich_string('B2',
-                            'This is ',
-                            red, 'red',
-                            ' and this is ',
-                            blue, 'blue',
-                            cell_format)
+
+worksheet.merge_range('D5:E5', "", cell_format)#-----DAY X
+worksheet.write('D5', u'DAY 2',dayCellFormat)
+
+worksheet.merge_range('G5:H5', "", cell_format)#-----DAY X
+worksheet.write('G5', u'DAY 3',dayCellFormat)
+
+worksheet.merge_range('J5:K5', "", cell_format)#-----DAY X
+worksheet.write('J5', u'DAY 4',dayCellFormat)
+##################################### END WRITING TO CELLS ###########################################################
 #close the worksheet
 workbook.close()
 
